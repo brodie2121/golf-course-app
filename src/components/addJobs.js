7d51da707825f247452edb55dadc57d918a5ca29
Brodie Brock <brodiebrock@gmail.com>
2019-07-15 18:27:55 -0400

    created the addjob component

---
+++ src/components/addJobs.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react'; 
+import { Route } from 'react-router-dom';
+
+class AddJob extends Component {
+    state = {
+        date : "",
+        firstJob : "",
+        commentsFirstJob : "",
+        secondJob : "",
+        commentsSecondJob : "",
+        employee_id : null
+    };
+
+    handleDateChange = e => {
+        this.setState({
+            date: e.target.value
+        });
+    };
+
+    handleFirstJobChange = e => {
+        this.setState({
+            firstJob: e.target.value
+        });
+    };
+
+    handleCommentsFirstJobChange = e => {
+        this.setState({
+            commentsFirstJob: e.target.value
+        });
+    };
+
+    handleSecondJobChange = e => {
+        this.setState({
+            secondJob: e.target.value
+        });
+    };
+
+    handleCommentsSecondJobChange = e => {
+        this.setState({
+            commentsSecondJob: e.target.value
+        });
+    };
+
+    handleEmployeeChange = e => {
+        this.setState({
+            employee_id: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const date = this.state.date;
+        const firstJob = this.state.firstJob;
+        const commentsFirstJob = this.state.commentsFirstJob;
+        const secondJob = this.state.secondJob;
+        const commentsSecondJob = this.state.commentsSecondJob;
+        const employee_id = this.state.employee_id;
+        const data = { date, firstJob, commentsFirstJob, secondJob, commentsSecondJob, employee_id };
+        const url = `http://localhost:3000/dailyjobboard/post/add`;
+        const response = fetch(url, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (response.status === 200) {
+                this.props.history.push("/");
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
+
+    render() {
+        return(
+            <>
+                <form on onSubmit={this.handleSubmit}>
+                    <label> Date: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleDateChange}
+                        name="date"
+                        value={this.state.date}
+                    />
+                    <label> first Job: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleFirstJobChange}
+                        name="firstJob"
+                        value={this.state.firstJob}
+                    />
+                    <label> Comments First Job: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleCommentsFirstJobChange}
+                        name="firstJob"
+                        value={this.state.commentsFirstJob}
+                    />
+                    <label> Second Job: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleSecondJobChange}
+                        name="secondJob"
+                        value={this.state.secondJob}
+                    />
+                    <label> Comments Second Job: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleCommentsSecondJobChange}
+                        name="commentsSecondJob"
+                        value={this.state.commentsSecondJob}
+                    />
+                    <label> Employee ID: </label>
+                    <input
+                        type="text"
+                        onChange={this.handleEmployeeChange}
+                        name="employee_id"
+                        value={this.state.employee_id}
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+            </>
+        );
+    }
+}
+
+export default AddJob;
